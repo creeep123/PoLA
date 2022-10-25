25e5c51e64e186428555ce2d3504ce44a99245cd
ChaseYin <yxf993993@yahoo.com>
2022-10-25 18:00:02 +0800

    clean useless codes

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -191,7 +191,6 @@ const ProfilePage = () => {
 
       <Modal
         open={open}
-        // title="Title"
         onOk={handleOk}
         onCancel={handleCancel}
         footer={[
@@ -212,10 +211,10 @@ const ProfilePage = () => {
           <p className="ant-upload-text">
             Click or drag file to this area to upload
           </p>
-          <p className="ant-upload-hint">
+          {/* <p className="ant-upload-hint">
             Support for a single or bulk upload. Strictly prohibit from
             uploading company data or other band files
-          </p>
+          </p> */}
         </Dragger>
         <div
           style={{
