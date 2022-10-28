41b0120266cc6d39b83bca1a2163987f4a72f5ff
David <DavidYangPersonal@outlook.com>
2022-10-28 15:37:38 +1100

    feat: take photo

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,38 +1,30 @@
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import { Breadcrumb, Layout, Menu, Button, List, Modal } from "antd";
-import { Col, Row } from "antd";
-import CourseList from "../../components/courseList";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { InboxOutlined, CameraOutlined } from "@ant-design/icons";
-import { message, Upload } from "antd";
+import { Layout, Button, Modal } from 'antd';
+import { Col, Row } from 'antd';
+import React, { useState, useRef } from 'react';
+import { InboxOutlined, CameraOutlined } from '@ant-design/icons';
+import { message, Upload } from 'antd';
+import { Camera } from 'react-camera-pro';
 const { Dragger } = Upload;
 const { Header, Content, Footer, Sider } = Layout;
 
 const props = {
-  name: "file",
+  name: 'file',
   multiple: false,
-  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-  method: "GET", //totally for mock
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  method: 'GET', //totally for mock
   onChange(info) {
     const { status } = info.file;
-    if (status !== "uploading") {
+    if (status !== 'uploading') {
       console.log(info.file, info.fileList);
     }
-    if (status === "done") {
+    if (status === 'done') {
       message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
+    } else if (status === 'error') {
       message.error(`${info.file.name} file upload failed.`);
     }
   },
   onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
+    console.log('Dropped files', e.dataTransfer.files);
   },
 };
 
@@ -42,9 +34,15 @@ const ProfilePage = () => {
 
   const [success, setSuccess] = useState(false);
 
+  // Camera Related Starts
+  const camera = useRef(null);
+  const [image, setImage] = useState(null);
+  const [useCamera, setUseCamera] = useState(false);
+
   const showModal = () => {
     setOpen(true);
   };
+  // Camera Related Ends
 
   const handleOk = () => {
     setLoading(true);
@@ -62,7 +60,7 @@ const ProfilePage = () => {
   return (
     <Content
       style={{
-        margin: "0 16px",
+        margin: '0 16px',
       }}
     >
       <div
@@ -74,10 +72,10 @@ const ProfilePage = () => {
       >
         <Row
           style={{
-            backgroundColor: "#F5F5F7",
-            borderRadius: "20px",
+            backgroundColor: '#F5F5F7',
+            borderRadius: '20px',
           }}
-          justify={"center"}
+          justify={'center'}
         >
           <Col span={10}>
             <Row>
@@ -86,25 +84,25 @@ const ProfilePage = () => {
                   width="80%"
                   src={
                     success
-                      ? require("./assets/successPic.png")
-                      : require("./assets/defultPic.png")
+                      ? require('./assets/successPic.png')
+                      : require('./assets/defultPic.png')
                   }
                   alt="DefaultPic"
                   style={{
-                    marginTop: "65px",
-                    marginLeft: "65px",
+                    marginTop: '65px',
+                    marginLeft: '65px',
                   }}
                 ></img>
               </Col>
             </Row>
-            <Row justify={"center"}>
+            <Row justify={'center'}>
               <Button
                 type="primary"
                 shape="round"
                 style={{
                   // marginLeft: "100px",
-                  marginTop: "30px",
-                  marginBottom: "40px",
+                  marginTop: '30px',
+                  marginBottom: '40px',
                 }}
                 onClick={showModal}
               >
@@ -116,38 +114,38 @@ const ProfilePage = () => {
           <Col
             span={13}
             style={{
-              backgroundColor: "#D9D9D9",
-              marginTop: "60px",
-              marginBottom: "100px",
-              borderRadius: "20px",
+              backgroundColor: '#D9D9D9',
+              marginTop: '60px',
+              marginBottom: '100px',
+              borderRadius: '20px',
             }}
           >
             <Row
               style={{
-                marginTop: "40px",
-                marginLeft: "40px",
-                fontSize: "20px",
-                fontWeight: "700",
+                marginTop: '40px',
+                marginLeft: '40px',
+                fontSize: '20px',
+                fontWeight: '700',
               }}
             >
               <p>Name: Jack</p>
             </Row>
             <Row
               style={{
-                marginTop: "40px",
-                marginLeft: "40px",
-                fontSize: "20px",
-                fontWeight: "700",
+                marginTop: '40px',
+                marginLeft: '40px',
+                fontSize: '20px',
+                fontWeight: '700',
               }}
             >
               <p>Id: 123456</p>
             </Row>
             <Row
               style={{
-                marginTop: "40px",
-                marginLeft: "40px",
-                fontSize: "20px",
-                fontWeight: "700",
+                marginTop: '40px',
+                marginLeft: '40px',
+                fontSize: '20px',
+                fontWeight: '700',
               }}
             >
               <p>Email: Jack@gmail.com</p>
@@ -158,31 +156,31 @@ const ProfilePage = () => {
           <Col
             span={12}
             style={{
-              marginTop: "30px",
-              paddingLeft: "85px",
-              backgroundColor: "#F5F5F7",
-              borderRadius: "20px",
+              marginTop: '30px',
+              paddingLeft: '85px',
+              backgroundColor: '#F5F5F7',
+              borderRadius: '20px',
             }}
           >
             <img
               width="90%"
-              src={require("./assets/hour.png")}
+              src={require('./assets/hour.png')}
               alt="DefaultPic"
               style={{
-                marginTop: "65px",
-                marginLeft: "5px",
-                marginBottom: "60px",
+                marginTop: '65px',
+                marginLeft: '5px',
+                marginBottom: '60px',
               }}
             ></img>
           </Col>
           <Col span={10}>
             <img
               width="100%"
-              src={require("./assets/YOUR STATISTICS SECTION.png")}
+              src={require('./assets/YOUR STATISTICS SECTION.png')}
               alt="DefaultPic"
               style={{
-                marginTop: "65px",
-                marginLeft: "65px",
+                marginTop: '65px',
+                marginLeft: '65px',
               }}
             ></img>
           </Col>
@@ -204,7 +202,7 @@ const ProfilePage = () => {
           </Button>,
         ]}
       >
-        <Dragger {...props} style={{ marginTop: "40px", marginBottom: "40px" }}>
+        <Dragger {...props} style={{ marginTop: '40px', marginBottom: '40px' }}>
           <p className="ant-upload-drag-icon">
             <InboxOutlined />
           </p>
@@ -218,45 +216,41 @@ const ProfilePage = () => {
         </Dragger>
         <div
           style={{
-            width: "100%",
-            height: "1px",
-            backgroundColor: "#F3F3F3",
-            marginBottom: "20px",
+            width: '100%',
+            height: '1px',
+            backgroundColor: '#F3F3F3',
+            marginBottom: '20px',
           }}
         ></div>
-        <button type="button" id="start-camera">open camera</button>
-        <button type="button" id="snapshot-btn">take picture</button>
-        <video id="preview-box" width="300" height="300" autoplay></video>
-        <canvas width="300" height="300"></canvas>
+
+        {useCamera ? (
+          <>
+            <div className="camera-video">
+              <Camera ref={camera} />
+            </div>
+            <Row justify="center" marginTop={"16px"} marginBottom={"16px"}>
+              <Button
+                type="primary"
+                onClick={() => setImage(camera.current.takePhoto())}
+                icon={<CameraOutlined />}
+              >
+                Take photo
+              </Button>
+            </Row>
+            <Row justify="center" marginTop={"16px"}>
+              <img style={{ width: '150px' }} src={image} />
+            </Row>
+          </>
+        ) : (
+          <Row justify="center">
+            <Button type="primary" onClick={() => setUseCamera(true)}>
+              Take photo
+            </Button>
+          </Row>
+        )}
       </Modal>
     </Content>
   );
 };
-
-const startCameraBtn = document.querySelector('#start-camera');  // 打开摄像头按钮
-const snapshotBtn = document.querySelector('#snapshot-btn');  // 拍照按钮
-const exportBtn = document.querySelector('#export-btn');  // 导出照片按钮
-const previewBox = document.querySelector('#preview-box');  // 预览区
-const canvas = document.querySelector('canvas');  // canvas用来显示拍摄的照片
-let imgData = null;  // 存储图片数据
-
-// 打开摄像头按钮点击
-startCameraBtn.addEventListener('click', () => {
-  // 申请摄像头权限
-  navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(stream => {
-    // 把媒体流直接传给 video 的 srcObject
-    previewBox.srcObject = stream;
-  }).catch(info => {
-    alert('无法获取摄像头权限：' + info);
-  });
-});
-
-// 拍照按钮点击
-snapshotBtn.addEventListener('click', () => {
-  // 绘制 2D 图像
-  canvas.getContext('2d').drawImage(previewBox, 0, 0, previewBox.width, previewBox.height);
-  // 把 canvas 的图像转换为 dataURL 数据
-  imgData = canvas.toDataURL('image/jpeg');
-});
 
 export default ProfilePage;
